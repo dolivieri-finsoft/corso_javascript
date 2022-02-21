5c710bc31133c3d9925e76b3d7c47fbefb8837cf
onyrix <olivieri@onyrix.com>
2022-02-21 10:59:01 +0100

    songs1

---
+++ esercizi/songs/songs.js
@@ -0,0 +1,22 @@
+// codice javascript di front-end dell'app SONGS
+
+/*
+
+PREREQUISITI: la app deve essere responsive e single page
+
+1. TODO nel corpo centrale deve esserci una tabella con id,title,author,composer
+2. TODO nel menu aggiungere un bottone o item "Add Song"
+3. TODO in ogni riga devono essere presenti due bottoni "Edit" e "Delete"
+4. TODO preparare delle fake response con dei file statici JSON in una cartella mockup
+5. TODO fare un form modale per inserimento o modifica di una canzone
+6. TODO implementare la Add Song (anche se solo simulata con fake response)
+7. TODO implementare la Edit Song (anche se solo simulata con fake response)
+8. TODO implementare la Delete Song (anche se solo simulata con fake response)
+*/
+
+window.addEventListener(
+	'DOMContentLoaded', 
+	function(event){
+
+	}
+);
