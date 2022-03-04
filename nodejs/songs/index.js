d23b75a21230a01d83b8714265c655e695db523d
onyrix <olivieri@onyrix.com>
2022-03-04 10:43:59 +0100

    crud todos

--- nodejs/songs/index.js
+++ nodejs/songs/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// 1. TODO creare un file JSON (copia del vecchio stub per iniziare) che sia il DB delle nostre canzoni
+// 2. TODO implementare la list servendo il file JSON stesso (cercare come servire file statici in express js) 
+// 3. TODO implementare la create: legge il json, aggiunge un oggetto all'array, lo salva e risponde come nello stub precedente
+// 4. TODO implementare la edit: legge il json, cerca/modifica un oggetto in array, lo salva e risponde come nello stub precedente
+// 5. TODO implementare la delete: legge il json, cerca e cancella oggetto, lo salva e risponde come nello stub precedente
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.send('Songs API - welcome aboard - available routings: create, delete, edit, list');
