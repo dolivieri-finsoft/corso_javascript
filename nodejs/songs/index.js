395dfdee10f17604ff76449df493b4410fa4bb28
onyrix <olivieri@onyrix.com>
2022-03-04 10:37:18 +0100

    crud base node

--- nodejs/songs/index.js
+++ nodejs/songs/index.js
@@ -1,1 +1,29 @@
-console.log("todo");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('Songs API - welcome aboard - available routings: create, delete, edit, list');
+});
+
+app.get('/create', (req, res) => {
+  res.send('hai chiamato la create');
+});
+
+app.get('/delete', (req, res) => {
+  res.send('hai chiamato la delete');
+});
+
+app.get('/edit', (req, res) => {
+  res.send('hai chiamato la edit');
+});
+
+app.get('/list', (req, res) => {
+  res.send('hai chiamato la list');
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
