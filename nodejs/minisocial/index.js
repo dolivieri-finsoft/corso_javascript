d2d7c9d5e11cf0ceccde5529078e9ad3af83fbc5
onyrix <olivieri@onyrix.com>
2022-03-07 09:40:39 +0100

    esercizio minisocial

---
+++ nodejs/minisocial/index.js
@@ -0,0 +1,11 @@
+console.log("minisocial started!");
+
+// TODOs del minisocial
+// 1. TODO fare un frontend che mostri sempre due cose: 1 form di inserimento di un pensiero (1 testo) e una lista dei pensieri precedentemente inseriti (in ordine INVERSO di inserimento)
+// 2. TODO fare un backend che implementi le seguenti operazioni:
+// 3. TODO list (dei pensieri)
+// 4. TODO create pensiero
+// 5. TODO delete pensiero
+// 6. TODO mostrare un gradimento (numero) del pensiero
+// 7. TODO gestire l'aggiunta di un +1 al gradimento di un pensiero
+// 8. TODO gestire un selettore di cambio ordinamento lista che consente di mostrare la list in due modi (ordine temporale INVERSO oppure GRADIMENTO DISCENDENTE)
